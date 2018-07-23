64bf37dd52333de545045d48018c99c87b611b58
Alexandre Rocca <alexandre.rocca@imag.fr>
2018-07-23 11:36:08 +0200

    Deleted reference to .._improved code

--- src/tensorial_form.jl
+++ src/tensorial_form.jl
@@ -20,33 +20,6 @@ function generate_tensor_form(implicitform::ImplicitForm{T})::Vector{T} where{T<
 end
 
 
-# function generate_tensor_form_improved(implicitform::ImplicitForm{T}, l::Vector{Int64})::Vector{T} where{T<:Number}
-#
-#     if implicitform.dim<= 0
-#         println("Empty implicit form")
-#         return implicitform.array[0]
-#     end
-#
-#     if implicitform.dim == 1
-#         return implicitform.array[1]
-#     end
-#
-#     e::Int64 = (l[1]+1)*(l[2]+1) #current array size
-#     pe::Int64 = e # previous array size
-#     berncoeffs = Array{Float64,1}(implicitform.ncoeffs)
-#     berncoeffs[1:e] = kron(implicitform.array[1], implicitform.array[2])
-#
-#     if implicitform.dim > 2
-#         @inbounds for i in 3:implicitform.dim
-#             e = e*(l[i]+1)
-#             @views berncoeffs[1:e]  = kron(berncoeffs[1:pe], implicitform.array[i])
-#             pe = e
-#         end
-#     end
-#
-#     return berncoeffs
-# end
-
 """
     multivariate_tensor(k::Vector{Int64}, l::Vector{Int64},
                           low::Vector{T}, high::Vector{T})::Vector{T} where {T<:Number}
