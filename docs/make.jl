1375743f6d39770450a5fd6abb3f25198a11a183
schillic <git@christianschilling.net>
2025-01-25 20:09:31 +0100

    use DocumenterCitations for bibliography

--- docs/make.jl
+++ docs/make.jl
@@ -1,20 +1,23 @@
-using Documenter, BernsteinExpansions
+using Documenter, BernsteinExpansions, DocumenterCitations
 
 DocMeta.setdocmeta!(BernsteinExpansions, :DocTestSetup,
                     :(using BernsteinExpansions); recursive=true)
 
+bib = CitationBibliography(joinpath(@__DIR__, "src", "refs.bib"); style=:alpha)
+
 makedocs(;
          sitename="BernsteinExpansions.jl",
          format=Documenter.HTML(; prettyurls=get(ENV, "CI", nothing) == "true",
-                                assets=["assets/aligned.css"]),
-         doctest=true,
+                                assets=["assets/aligned.css", "assets/citations.css"]),
          pagesonly=true,
+         plugins=[bib],
          pages=["Home" => "index.md",
                 "Manual" => Any["Introduction" => "man/introduction.md",
                                 "Range Enclosure" => "man/range.md",
                                 "Benchmarks" => "man/benchmarks.md"],
                 "Library" => Any["Types" => "lib/types.md",
                                  "Methods" => "lib/methods.md"],
+                "Bibliography" => "bibliography.md",
                 "About" => "about.md"])
 
 deploydocs(; repo="github.com/JuliaReach/BernsteinExpansions.jl.git",
