211f2384c4bbbd1755225aa9a2fcb7af38196ae9
schillic <git@christianschilling.net>
2026-03-02 00:01:20 +0100

    better ExplicitImports usage

--- test/quality_assurance.jl
+++ test/quality_assurance.jl
@@ -1,20 +1,11 @@
 using BernsteinExpansions, Test
-import Aqua
+import Aqua, ExplicitImports
 
-import Pkg
-@static if VERSION >= v"1.6"  # TODO make explicit test requirement
-    Pkg.add("ExplicitImports")
-    import ExplicitImports
-
-    @testset "ExplicitImports tests" begin
-        @test isnothing(ExplicitImports.check_all_explicit_imports_are_public(BernsteinExpansions))
-        @test isnothing(ExplicitImports.check_all_explicit_imports_via_owners(BernsteinExpansions))
-        @test isnothing(ExplicitImports.check_all_qualified_accesses_are_public(BernsteinExpansions))
-        @test isnothing(ExplicitImports.check_all_qualified_accesses_via_owners(BernsteinExpansions))
-        @test_broken isnothing(ExplicitImports.check_no_implicit_imports(BernsteinExpansions))  # related to IntervalArithmetic
-        @test isnothing(ExplicitImports.check_no_self_qualified_accesses(BernsteinExpansions))
-        @test isnothing(ExplicitImports.check_no_stale_explicit_imports(BernsteinExpansions))
-    end
+@testset "ExplicitImports tests" begin
+    # related to reexporting IntervalArithmetic
+    ignores_no_implicit_imports = (:IntervalArithmetic, :Interval, :inf, :sup)
+    ExplicitImports.test_explicit_imports(BernsteinExpansions;
+                                          no_implicit_imports=(ignore=ignores_no_implicit_imports,))
 end
 
 @testset "Aqua tests" begin
